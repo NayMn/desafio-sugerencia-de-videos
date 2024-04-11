80ad385e165d776d56f24635ccb35463c39896bf
najira <najira_daniela@hotmail.com>
2024-04-10 22:49:26 -0400

    creacion de clases

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,4 +10,29 @@ const funcionAutomatica = (() => {
     }
 })();
 
+class Multimedia {
+    constructor(url) {
+        this._url = url;
+    }
+    get getUrl() {
+        return this._url;
+    }
+    setInicio() {
+        return 'Este metodo es para realizar un cambio en la URL del video'
+    }
+};
 
+class Reproductor extends Multimedia {
+    constructor(id, url) {
+        super(url);
+        this._id = id;
+    }
+    playMultimedia() {
+        funcionAutomatica.funcionPublica(this._url, this._id)
+    }
+    setInicio(tiempo) {
+        this._id.setAttribute('src', `${this._url}?start=${tiempo}`);
+
+    }
+
+};
